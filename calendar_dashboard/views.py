218217add80ea9e3464fffd2f2ed110e7a25e6c4
Moses Chawawa <moseschawawa@gmail.com>
2022-02-09 17:47:44 +0200

    Added calendar buttons

--- calendar_dashboard/views.py
+++ calendar_dashboard/views.py
@@ -1,9 +1,9 @@
+import calendar
 import imp
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
@@ -27,7 +27,12 @@ class CalendarView(generic.ListView):
         cal = Calendar(d.year, d.month)
 
         # Call the formatmonth method, which returns our calendar as a table
+        
         html_cal = cal.formatmonth(withyear=True)
+        d = get_date(self.request.GET.get('month', None))
+        
+        context['prev_month'] = prev_month(d)
+        context['next_month'] = next_month(d)
         context['calendar'] = mark_safe(html_cal)
         return context
 
@@ -37,3 +42,18 @@ def get_date(req_day):
         year, month = (int(x) for x in req_day.split('-'))
         return date(year, month, day=1)
     return datetime.today()
+
+
+def prev_month(d):
+    first = d.replace(day=1)
+    prev_month = first - timedelta(days=1)
+    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    return month
+
+
+def next_month(d):
+    days_in_month = calendar.monthrange(d.year, d.month)[1]
+    last = d.replace(day=days_in_month)
+    next_month = last + timedelta(days=1)
+    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    return month
