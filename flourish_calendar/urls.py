7187ebb0c78dc1052bfe6e1a869a8d63257410ad
Moses Chawawa <moseschawawa@gmail.com>
2022-02-24 14:30:38 +0200

    Added urls

--- flourish_calendar/urls.py
+++ flourish_calendar/urls.py
@@ -6,6 +6,4 @@ from . import views
 app_name = 'flourish_calendar'
 urlpatterns = [
     path('', views.CalendarView.as_view(), name='calendar'),
-    path('event/new/', views.event, name='event_new'),
-    path('event/edit/<event_id>/', views.event, name='event_edit'),
 ]
