e3591645a306857e3614159e6d251890de2a2386
Moses Chawawa <moseschawawa@gmail.com>
2022-03-07 11:16:07 +0200

    fixed security issue

--- flourish_calendar/settings.py
+++ flourish_calendar/settings.py
@@ -25,9 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r2+kl0ci5#p_h4(e4edde8zko5ch%pp^jc-3z%rvh@fzmtv%09'
-
 SITE_ID = 40
 
 DEFAULT_STUDY_SITE = 40
