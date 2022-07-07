64526337813c9725ec212b9a9f8c3ea36e4dd7e4
Moses Chawawa <moseschawawa@gmail.com>
2022-07-07 10:37:08 +0200

    Added option to add participant note and prepopulating functionality

--- flourish_calendar/models/participant_note.py
+++ flourish_calendar/models/participant_note.py
@@ -6,7 +6,7 @@ from ..choices import COLORS, REMINDER_STATUS
 
 class ParticipantNote(BaseUuidModel):
     date = models.DateField()
-    subject_identifier = models.CharField(max_length=20)
+    subject_identifier = models.CharField(max_length=19)
     title = models.CharField(max_length=20)
     description = models.TextField(blank=True, null=True)
     color = models.CharField(max_length=20, blank=True, null=True, choices=COLORS)
