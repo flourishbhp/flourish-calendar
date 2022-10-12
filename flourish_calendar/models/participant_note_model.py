aab3321271eca41b779102df2b389241137fdb12
Moses Chawawa <moseschawawa@gmail.com>
2022-10-12 16:28:38 +0200

    CHANGE: model refactor

--- flourish_calendar/models/participant_note_model.py
+++ flourish_calendar/models/participant_note_model.py
@@ -1,14 +1,15 @@
 
 from django.db import models
+from django.forms import ValidationError
 from django.utils import timezone
 from edc_base.model_mixins import BaseUuidModel
-
 from ..choices import COLORS, REMINDER_STATUS
 
 
 class ParticipantNote(BaseUuidModel):
     date = models.DateField()
-    subject_identifier = models.CharField(max_length=19)
+    subject_identifier = models.CharField(
+        max_length=19)
     title = models.CharField(max_length=20)
     description = models.TextField(blank=True, null=True)
     color = models.CharField(max_length=20, blank=True, null=True, choices=COLORS)
