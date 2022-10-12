f1bbe26daf211d078265ddc3608f93e4d720ea06
Moses Chawawa <moseschawawa@gmail.com>
2022-10-12 15:53:25 +0200

    CHANGE : Minor refactor

---
+++ flourish_calendar/models/appointment_status_model.py
@@ -0,0 +1,13 @@
+from django.db import models
+from edc_base.model_mixins import BaseUuidModel
+from ..choices import APPT_COLOR
+
+class AppointmentStatus(BaseUuidModel):
+    '''
+    Model for keeping track of appointment colors based on the subject identifier
+    and visit code
+    '''
+    subject_identifier = models.CharField(max_length=20, unique=True)
+    visit_code = models.CharField(max_length=10)
+    color = models.CharField(max_length=10, choices=APPT_COLOR)
+    appt_date = models.DateTimeField()
