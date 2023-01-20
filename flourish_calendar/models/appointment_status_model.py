181c2a7263293a92530914cb990923081bfe6fc4
McTurner45 <58169969+nmunatsi@users.noreply.github.com>
2023-01-20 12:05:24 +0200

    Fix: duplicate identifiers error on the db

--- flourish_calendar/models/appointment_status_model.py
+++ flourish_calendar/models/appointment_status_model.py
@@ -7,7 +7,7 @@ class AppointmentStatus(BaseUuidModel):
     Model for keeping track of appointment colors based on the subject identifier
     and visit code
     '''
-    subject_identifier = models.CharField(max_length=20, unique=True)
+    subject_identifier = models.CharField(max_length=20)
     visit_code = models.CharField(max_length=10)
     color = models.CharField(max_length=10, choices=APPT_COLOR)
     appt_date = models.DateTimeField()
