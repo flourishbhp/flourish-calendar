50b1f0118160a2576a48c8dd315ce809afee097e
Moses Chawawa <moseschawawa@gmail.com>
2023-11-08 12:04:10 +0200

    Fix calendar

--- flourish_calendar/models/signals.py
+++ flourish_calendar/models/signals.py
@@ -1,12 +1,17 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
 from flourish_calendar.utils import ReminderDuplicator
 from .reminder_model import Reminder
 
 
+@receiver(pre_save, sender=Reminder)
+def reminder_pre_save(sender, instance, *args, **kwargs):
+    Reminder.objects.filter(
+        title=instance.title,
+        note=instance.note, ).delete()
+
+
 @receiver(post_save, sender=Reminder)
 def reminder_post_save(sender, instance, created, **kwargs):
-    if instance and not instance.is_repeated and created:
-        ReminderDuplicator(instance).repeat()
-        instance.delete()
+    ReminderDuplicator(instance).repeat()
